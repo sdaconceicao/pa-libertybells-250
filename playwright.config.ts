b3983df6a4183645a27c238029d7be11f7eddb31
sdaconceicao <daconcei472@gmail.com>
2026-06-11 14:15:38 -0400

    caching

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,12 @@ export default defineConfig({
 	testDir: "./e2e",
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 1 : 0,
-	reporter: process.env.CI ? "github" : "list",
+	reporter: process.env.CI
+		? [
+				["github"],
+				["html", { open: "never" }],
+			]
+		: "list",
 	use: {
 		baseURL,
 		trace: "on-first-retry",
